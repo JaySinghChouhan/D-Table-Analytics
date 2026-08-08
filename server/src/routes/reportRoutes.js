376b847de18014356bc8536fed2d8e7e00c2ea29
Jay Singh Chouhan <jaysinghchouhan009@gmail.com>
2026-08-08 16:39:47 +0530

    update the backend code

--- server/src/routes/reportRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getDailyReport } = require('../controllers/reportController');
-const { protect, authorize } = require('../middleware/auth');
-
-const router = express.Router();
-
-router.get('/daily', protect, authorize('employee', 'manager', 'admin'), getDailyReport);
-
-module.exports = router;
